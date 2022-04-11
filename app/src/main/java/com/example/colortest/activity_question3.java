993b17d3f7e8c579f1233effb71225a2b40954e4
백다미 <100damii@naver.com>
2022-04-11 09:23:18 +0900

    make question*.xml

--- app/src/main/java/com/example/colortest/activity_question3.java
+++ app/src/main/java/com/example/colortest/activity_question3.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TestQuestion extends AppCompatActivity {
+public class activity_question3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question1);
+        setContentView(R.layout.activity_question3);
     }
 }
--- app/src/main/java/com/example/colortest/question2.java
+++ app/src/main/java/com/example/colortest/question2.java
@@ -9,6 +9,6 @@ public class question2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.question2);
+        setContentView(R.layout.activity_question2);
     }
 }
